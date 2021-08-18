4a3c3786c600f40e1967a85ebea2decaa71797bf
kolenkorov <kolenkorov.andrey@gmail.com>
2021-08-18 17:12:27 +0300

    Add files via upload

---
+++ magicdrop/js/main.js
@@ -0,0 +1,3 @@
+$('select').styler();
+$('.jq-selectbox__trigger').addClass('blue_btn');
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoJ3NlbGVjdCcpLnN0eWxlcigpO1xyXG4kKCcuanEtc2VsZWN0Ym94X190cmlnZ2VyJykuYWRkQ2xhc3MoJ2JsdWVfYnRuJyk7Il0sImZpbGUiOiJtYWluLmpzIn0=
