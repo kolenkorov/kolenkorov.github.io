587fa188c21c3ffb6f62390597799ed69ed8405d
kolenkorov <kolenkorov.andrey@gmail.com>
2021-09-21 16:49:11 +0300

    Add files via upload

--- magicdrop/js/main.js
+++ magicdrop/js/main.js
@@ -1,5 +1,5 @@
 $('select').styler();
-$('.jq-selectbox__trigger').addClass('blue_btn');
+$('.jq-selectbox__trigger').addClass('btn blue_btn');
 
 $('.cases__item-fav').on('click', function(){
 	$(this).toggleClass('active')
@@ -11,4 +11,4 @@ $('.cases__item-fav').on('click', function(){
 		$('.aside').toggleClass('active')
 	});
 /* Menu end*/
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoJ3NlbGVjdCcpLnN0eWxlcigpO1xyXG4kKCcuanEtc2VsZWN0Ym94X190cmlnZ2VyJykuYWRkQ2xhc3MoJ2JsdWVfYnRuJyk7XHJcblxyXG4kKCcuY2FzZXNfX2l0ZW0tZmF2Jykub24oJ2NsaWNrJywgZnVuY3Rpb24oKXtcclxuXHQkKHRoaXMpLnRvZ2dsZUNsYXNzKCdhY3RpdmUnKVxyXG59KTtcclxuXHJcbi8qIE1lbnUgKi9cclxuXHQkKCcuaGVhZGVyX19idXJnZXInKS5vbignY2xpY2snLCBmdW5jdGlvbigpe1xyXG5cdFx0JCh0aGlzKS50b2dnbGVDbGFzcygnYWN0aXZlJylcclxuXHRcdCQoJy5hc2lkZScpLnRvZ2dsZUNsYXNzKCdhY3RpdmUnKVxyXG5cdH0pO1xyXG4vKiBNZW51IGVuZCovIl0sImZpbGUiOiJtYWluLmpzIn0=
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJtYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoJ3NlbGVjdCcpLnN0eWxlcigpO1xyXG4kKCcuanEtc2VsZWN0Ym94X190cmlnZ2VyJykuYWRkQ2xhc3MoJ2J0biBibHVlX2J0bicpO1xyXG5cclxuJCgnLmNhc2VzX19pdGVtLWZhdicpLm9uKCdjbGljaycsIGZ1bmN0aW9uKCl7XHJcblx0JCh0aGlzKS50b2dnbGVDbGFzcygnYWN0aXZlJylcclxufSk7XHJcblxyXG4vKiBNZW51ICovXHJcblx0JCgnLmhlYWRlcl9fYnVyZ2VyJykub24oJ2NsaWNrJywgZnVuY3Rpb24oKXtcclxuXHRcdCQodGhpcykudG9nZ2xlQ2xhc3MoJ2FjdGl2ZScpXHJcblx0XHQkKCcuYXNpZGUnKS50b2dnbGVDbGFzcygnYWN0aXZlJylcclxuXHR9KTtcclxuLyogTWVudSBlbmQqLyJdLCJmaWxlIjoibWFpbi5qcyJ9
